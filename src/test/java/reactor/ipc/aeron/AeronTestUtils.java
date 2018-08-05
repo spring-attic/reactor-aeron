77f16a9e000657574d8ddb6713378eabc936257a
ronen <ronen@ronen-pc.mshome.net>
2018-08-05 21:21:11 +0300

    update aeron version 1.10.4

--- src/test/java/reactor/ipc/aeron/AeronTestUtils.java
+++ src/test/java/reactor/ipc/aeron/AeronTestUtils.java
@@ -12,10 +12,8 @@ class AeronTestUtils {
 
     static void setAeronEnvProps() {
         String bufferLength = String.valueOf(128 * 1024);
-        System.setProperty(MediaDriver.DIRS_DELETE_ON_START_PROP_NAME, "true");
 
         System.setProperty(Configuration.TERM_BUFFER_LENGTH_PROP_NAME, bufferLength);
-        System.setProperty(Configuration.TERM_BUFFER_MAX_LENGTH_PROP_NAME, bufferLength);
         System.setProperty(Configuration.IPC_TERM_BUFFER_LENGTH_PROP_NAME, bufferLength);
         System.setProperty(Configuration.COUNTERS_VALUES_BUFFER_LENGTH_PROP_NAME, bufferLength);
         System.setProperty(Configuration.PUBLICATION_LINGER_PROP_NAME,
