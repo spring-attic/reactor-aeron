7df8ff9272394322cb946f61f38c51c9629cd669
ronen <ronen.hamias@gmail.com>
2019-01-08 12:23:30 +0200

    fix doc

--- reactor-aeron-core/src/main/java/reactor/aeron/AeronEventLoop.java
+++ reactor-aeron-core/src/main/java/reactor/aeron/AeronEventLoop.java
@@ -38,7 +38,7 @@ public final class AeronEventLoop implements OnDisposable {
   /**
    * Constructor.
    *
-   * @param name name
+   * @param name of thread.
    * @param workerId worker id
    * @param groupId id of parent {@link AeronEventLoopGroup}
    * @param idleStrategy {@link IdleStrategy} instance for this event loop
