9a838ff4a02d407a794ac7d1aaab9f3e1a7868e0
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-01-21 18:11:48 +0200

    Updated ClientServerSends and ServerServerSends to use netty byte buf

--- reactor-aeron-tools/src/main/java/reactor/aeron/demo/ClientServerSends.java
+++ reactor-aeron-tools/src/main/java/reactor/aeron/demo/ClientServerSends.java
@@ -1,7 +1,14 @@
 package reactor.aeron.demo;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.nio.charset.Charset;
+import org.agrona.DirectBuffer;
 import reactor.aeron.AeronClient;
 import reactor.aeron.AeronResources;
+import reactor.core.CoreSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxOperator;
 
 public class ClientServerSends {
 
@@ -20,6 +27,7 @@ public class ClientServerSends {
                   connection
                       .inbound()
                       .receive()
+                      .as(ByteBufFlux::create)
                       .asString()
                       .log("receive")
                       .then(connection.onDispose()))
@@ -31,6 +39,32 @@ public class ClientServerSends {
     } finally {
       resources.dispose();
       resources.onDispose().block();
+    }
+  }
+
+  static class ByteBufFlux extends FluxOperator<ByteBuf, ByteBuf> {
+
+    public ByteBufFlux(Flux<? extends ByteBuf> source) {
+      super(source);
+    }
+
+    public static ByteBufFlux create(Flux<DirectBuffer> directBufferFlux) {
+      return new ByteBufFlux(
+          directBufferFlux.map(
+              buffer -> {
+                byte[] bytes = new byte[buffer.capacity()];
+                buffer.getBytes(0, bytes);
+                return Unpooled.copiedBuffer(bytes);
+              }));
+    }
+
+    @Override
+    public void subscribe(CoreSubscriber<? super ByteBuf> actual) {
+      source.subscribe(actual);
+    }
+
+    public Flux<String> asString() {
+      return map(buffer -> buffer.toString(Charset.defaultCharset()));
     }
   }
 }
