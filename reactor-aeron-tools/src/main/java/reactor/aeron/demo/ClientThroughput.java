81e7859b7c62047a5aa18d306054f997c330b29b
segabriel <sergiy.gabriel@jivygroup.com>
2019-01-23 23:47:36 +0200

    Added rate reporter as a separate class, polishing

--- reactor-aeron-tools/src/main/java/reactor/aeron/demo/ClientThroughput.java
+++ reactor-aeron-tools/src/main/java/reactor/aeron/demo/ClientThroughput.java
@@ -13,7 +13,7 @@ public class ClientThroughput {
    *
    * @param args program arguments.
    */
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     AeronResources aeronResources =
         new AeronResources()
             .useTmpDir()
@@ -22,25 +22,21 @@ public class ClientThroughput {
             .start()
             .block();
 
-    try {
-      ByteBuffer buffer = ByteBuffer.allocate(1024);
+    ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-      AeronClient.create(aeronResources)
-          .options("localhost", 13000, 13001)
-          .handle(
-              connection ->
-                  connection
-                      .outbound()
-                      .sendBuffer(Flux.range(0, Integer.MAX_VALUE).map(i -> buffer))
-                      .then(connection.onDispose()))
-          .connect()
-          .block();
-
-      System.out.println("main completed");
-      Thread.currentThread().join();
-    } finally {
-      aeronResources.dispose();
-      aeronResources.onDispose().block();
-    }
+    AeronClient.create(aeronResources)
+        .options("localhost", 13000, 13001)
+        .handle(
+            connection ->
+                connection
+                    .outbound()
+                    .sendBuffer(Flux.range(0, Integer.MAX_VALUE).map(i -> buffer))
+                    .then(connection.onDispose()))
+        .connect()
+        .block()
+        .onDispose()
+        .doFinally(s -> aeronResources.dispose())
+        .then(aeronResources.onDispose())
+        .block();
   }
 }
