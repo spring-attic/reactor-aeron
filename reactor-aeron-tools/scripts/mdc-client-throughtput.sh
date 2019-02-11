6e7de90787a31b4cd9172dab7dbc0cc5dfba1a65
segabriel <sergiy.gabriel@jivygroup.com>
2019-02-11 17:13:36 +0200

    Added pure client-server throughput benchmarks, removed redundant

---
+++ reactor-aeron-tools/scripts/mdc-client-throughtput.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR=$(dirname $0)
+TARGET_DIR="${SCRIPTS_DIR%/*}"/target
+JAR_FILE=$(ls ${TARGET_DIR} |grep jar)
+
+${JAVA_HOME}/bin/java \
+    -cp ${TARGET_DIR}/${JAR_FILE}:${TARGET_DIR}/lib/* \
+    -XX:BiasedLockingStartupDelay=0 \
+    -Djava.net.preferIPv4Stack=true \
+    -Daeron.mtu.length=16k \
+    -Daeron.socket.so_sndbuf=2m \
+    -Daeron.socket.so_rcvbuf=2m \
+    -Daeron.rcv.initial.window.length=2m \
+    -Daeron.term.buffer.sparse.file=false \
+    -Daeron.threading.mode=SHARED \
+    -Dagrona.disable.bounds.checks=true \
+    -Dreactor.aeron.sample.embeddedMediaDriver=true \
+    -Dreactor.aeron.sample.exclusive.publications=true \
+    -Dreactor.aeron.sample.messageLength=1024 \
+    -Dreactor.aeron.sample.idle.strategy=yielding \
+    ${JVM_OPTS} reactor.aeron.demo.pure.ClientThroughput
