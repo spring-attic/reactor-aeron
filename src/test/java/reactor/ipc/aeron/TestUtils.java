06acdf3931c2905136cb27fb68a2597dfdd2a10a
Stephane Maldini <smaldini@pivotal.io>
2019-03-25 22:21:33 -0700

    Archive Reactor Aeron

--- src/test/java/reactor/ipc/aeron/TestUtils.java
+++
@@ -1,9 +0,0 @@
-package reactor.ipc.aeron;
-
-public class TestUtils {
-
-    public static void log(String msg) {
-        System.out.println(Thread.currentThread().getName() + " - " + msg);
-    }
-
-}
